09a581d6418fb91ca90053c98cab1a3c05b57ad9
Nexxon-byte <37150234+Nexxon-byte@users.noreply.github.com>
2023-02-20 05:22:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,36 @@
-import requests
-import io
-import time
-import picamera
+mport requests
+import cv2
 
 # Öffnen der Kamera und Konfiguration der Auflösung
-width = 640
-height = 480
-
-camera = picamera.PiCamera(resolution=(width, height))
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Schleife zum Lesen von Frames aus der Kamera und Senden an den Server
 while True:
     # Lesen des aktuellen Frames aus der Kamera
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg', use_video_port=True)
-    img_bytes = stream.getvalue()
+    ret, frame = cap.read()
+
+    # Konvertieren des Frames in ein Byte-Array
+    ret, buffer = cv2.imencode('.jpg', frame)
+    jpg_as_text = buffer.tobytes()
 
     # Definieren des API-Endpunkts des Servers und der zu sendenden Daten
-    url = 'http://92.168.1.93:8000/api'
-    files = {'image': ('image.jpg', img_bytes)}
+    url = 'http://localhost:8000/api'
+    files = {'image': ('image.jpg', jpg_as_text)}
 
-    # Senden der Daten an den Server
-    response = requests.post(url, files=files)
+    # Senden der Daten an den Server und Warten auf die Antwort
+    response = None
+    while not response:
+        response = requests.post(url, files=files)
 
-    # Drucken der Server-Antwort, falls vorhanden
-    if response.content:
-        print(response.content)
+    # Drucken der Server-Antwort
+    print(response.text)
 
     # Warten auf Tastendruck zum Beenden der Schleife
-    time.sleep(0.3)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Freigabe der Kamera und Schließen des Skripts
+cap.release()
+cv2.destroyAllWindows()
